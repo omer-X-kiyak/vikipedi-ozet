085d5a4724a759d99ddac2476d659179ead6498e
omer-X-kiyak <89635390+omer-X-kiyak@users.noreply.github.com>
2023-02-20 14:00:27 +0300

    Update Vikipedi.py

--- Vikipedi.py
+++ Vikipedi.py
@@ -16,15 +16,22 @@ while True:
     # Yeni bir Word belgesi oluştur
     doc = docx.Document()
 
+    # core_properties özelliğine ait author alanını ayarla
+    doc.core_properties.author = "omer-x-kiyak"
+
     # Her bir arama sonucu için özetleri belgeye ekle
     for item in result:
         try:
-            # Başlık ekle
-            doc.add_heading(item, level=1)
-            # Özet ekle
-            doc.add_paragraph(wikipedia.page(item).summary)
+            # Sayfayı getir ve özetini ekle
+            page = wikipedia.page(item)
+            if page.title:
+                doc.add_heading(page.title, level=1)
+                doc.add_paragraph(page.summary)
         except wikipedia.exceptions.DisambiguationError as e:
             print(e.options)
+            continue
+        except wikipedia.exceptions.PageError as e:
+            print(f"Sayfa bulunamadı: {e}")
             continue
 
     if result:
